6d679777979a90e6428a215abd9f5d33683043a6
suresh <psureshsaravanan@gmail.com>
2021-10-28 19:01:53 +0530

    doine

---
+++ TestInLinux/src/main/java/com/base/test/WebDriverBase.java
@@ -0,0 +1,24 @@
+package com.base.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverBase {
+
+	public static void main(String[] args) {
+		System.out.println("******************");
+		System.out.println("-----ChromeDriver Execution starts----");
+		System.out.println("System directory is -> "+System.getProperty("user.dir"));
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/Driver/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://google.co.in");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.quit(); 
+		System.out.println("Execution success--------");
+	}
+}
---
+++ TestInLinux/src/test/java/com/org/web/WebDriverBase.java
@@ -0,0 +1,23 @@
+package com.org.web;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverBase {
+
+	public static void main(String[] args) {
+		System.out.println("******************");
+		System.out.println("-----ChromeDriver Execution starts----");
+		System.out.println("System directory is -> "+System.getProperty("user.dir"));
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/Driver/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://google.co.in");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.quit(); 
+	}
+}
