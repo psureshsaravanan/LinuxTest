36ccca22e15f3064388b572b46b20028a9d627c5
psureshsaravanan <44559760+psureshsaravanan@users.noreply.github.com>
2021-11-03 10:53:55 +0530

    Update WebDriverBase.java

--- TestInLinux/src/main/java/com/base/test/WebDriverBase.java
+++ TestInLinux/src/main/java/com/base/test/WebDriverBase.java
@@ -10,7 +10,7 @@ public class WebDriverBase {
 		System.out.println("******************");
 		System.out.println("-----ChromeDriver Execution starts----");
 		System.out.println("System directory is -> "+System.getProperty("user.dir"));
-		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/Driver/1chromedriver");
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"1chromedriver.exe");
 		//WebDriverManager.chromedriver().setup();
 		//WebDriver driver=new ChromeDriver();
 		ChromeOptions options = new ChromeOptions();
